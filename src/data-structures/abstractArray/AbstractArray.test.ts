c7c958b92edea61f4fd6d978f2ae6b8f8dfc4ca6
Thanh TRAN <ttranpro32@gmail.com>
2019-10-27 17:23:55 +0100

    Add tests for AbstractArray

---
+++ src/data-structures/abstractArray/AbstractArray.test.ts
@@ -0,0 +1,13 @@
+import AbstractArray from "./AbstractArray";
+
+describe("AbstractArray", () => {
+  it("should init array", () => {
+    const arr = new AbstractArray([1, 2, 3]);
+    expect(arr.getItems()).toEqual([1, 2, 3]);
+  });
+
+  it("should return array length", () => {
+    const arr = new AbstractArray([1, 2, 3]);
+    expect(arr.length()).toEqual(3);
+  });
+});
