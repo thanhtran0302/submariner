222896ff211a47c19cca7f2c6eda04af14208cc0
Thanh TRAN <ttranpro32@gmail.com>
2019-10-27 16:53:53 +0100

    Add stack data structure

---
+++ src/data-structures/stack/Stack.test.ts
@@ -0,0 +1,41 @@
+import Stack from "./Stack";
+
+describe("Stack", () => {
+  it("should init stack with an empty array", () => {
+    const stack = new Stack();
+
+    expect(stack.getItems()).toEqual([]);
+  });
+
+  it("should init stack with array of number", () => {
+    const stack = new Stack([1, 2, 3]);
+
+    expect(stack.getItems()).toEqual([1, 2, 3]);
+  });
+
+  it("should return exact stack size", () => {
+    const stack = new Stack([1, 2, 3]);
+
+    expect(stack.length()).toEqual(3);
+  });
+
+  it("should return popped element", () => {
+    const stack = new Stack([1, 2, 3]);
+
+    expect(stack.pop()).toEqual(3);
+  });
+
+  it("should resized popped stack", () => {
+    const stack = new Stack([1, 2, 3]);
+
+    stack.pop();
+    expect(stack.length()).toEqual(2);
+  });
+
+  it("should init stack with array of custom type", () => {
+    type CustomType = boolean | number;
+    const stack = new Stack<CustomType>([true, 1, false]);
+
+    expect(stack.getItems()).toEqual([true, 1, false]);
+  });
+});
---
+++ src/data-structures/stack/Stack.ts
@@ -0,0 +1,29 @@
+export default class Stack<T = number> {
+  private _size: number = 0;
+  private _items: T[] = [];
+
+  constructor(items?: T[]) {
+    if (items) {
+      this._items = items;
+      this._size = items.length;
+    }
+  }
+
+  public length() {
+    return this._size;
+  }
+
+  public push(item: T) {
+    this._items.push(item);
+    this._size += 1;
+  }
+
+  public pop() {
+    this._size -= 1;
+    return this._items.pop();
+  }
+
+  public getItems(): T[] {
+    return this._items;
+  }
+}
