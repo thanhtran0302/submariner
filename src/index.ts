ba0b89d7e995906f5650266571ff1efff828a80f
Thanh TRAN <ttranpro32@gmail.com>
2019-10-29 19:35:34 +0100

    Export double linked list

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
-export { default as Queue } from "./data-structures/queue/Queue";
-export { default as Stack } from "./data-structures/stack/Stack";
+export { default as Queue } from './data-structures/queue/Queue';
+export { default as Stack } from './data-structures/stack/Stack';
+export {
+  default as DoubleLinkedList,
+  LinkNode
+} from './data-structures/doubleLinkedList/DoubleLinkedList';
